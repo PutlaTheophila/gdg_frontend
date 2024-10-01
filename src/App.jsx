18f5f6dbb31cd7b51f10d2b6111651953f1e8c42
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-01 17:11:03 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import {
 import HomeLayout from "./components/HomeLayout.jsx";
 import Maps from "./components/Maps.jsx";
 import { loader as mapsDataLoader } from "./components/Maps.jsx";
+import DashboardContent from "./components/index.jsx";
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<HomeLayout/>}>
+      <Route index element={<DashboardContent/>}/>
       <Route path='/maps' loader={mapsDataLoader} element={<Maps/>}/>
       <Route path='/coordinates' element = {<h1>hello form coordinates section</h1>}/>
     </Route>
